59dd77b543b2e5c988af7cca7569d738088d0f7a
xcatliu <xcatliu@gmail.com>
2015-07-01 23:23:30 +0800

    Add src/index

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var getDefaultPage = require('reacat-plugin-front-matter/lib/getDefaultPage');
+var objectAssign = require('object-assign');
+
+var extnames = ['.jsx', '.react'];
+
+function parseReact() {
+  require("babel/register")({
+    only: path.resolve(this.cwd, this.config.source_dir),
+    extensions: extnames
+  });
+  Object.keys(this.sources).forEach((function(filePath) {
+    var source = this.sources[filePath];
+    var extname = path.extname(filePath).toLowerCase();
+    if (extnames.indexOf(extname) === -1) return;
+    var Content = require(filePath);
+    source.page = objectAssign(getDefaultPage.call(this, source, ''), Content.frontMatter);
+    source.page.Content = Content;
+    this.log.verbose('parseReact', source.page.Content);
+  }).bind(this));
+}
+
+module.exports = parseReact;
